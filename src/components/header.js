dc32c20d92d48f524e2edd9f67a7e3c5818a063a
slavik <slavik.zaliotnyi@gmail.com>
2023-01-04 20:10:43 +0200

    fixed header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,16 @@
 import {Component} from '../core/component'
 
+const body = document.querySelector('.body');
+
 export class Header extends Component{
   constructor(id){
     super(id);
   }
   init(){
+    if(localStorage.getItem('visited')){
+      this.hide();
+      body.classList.remove("overflow");
+    }
     const btn = this.$el.querySelector('.js-start');
     btn.addEventListener("click", buttonHandler.bind(this));
   }
@@ -12,4 +18,6 @@ export class Header extends Component{
 
 function buttonHandler() {
   this.hide();
+  body.classList.remove('overflow');
+  localStorage.setItem('visited','true');
 } 
